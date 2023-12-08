957642474cd1100b58f1fc30ca19aacb4474d840
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-12-08 18:38:44 +0900

    header updated

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -9,7 +9,6 @@ import { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
 import HeaderComponent from './HeaderComponent'
 
-
 export async function Header() {
   let header: Header | null = null
 
